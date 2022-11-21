f2831fd212db9ae179e494bfa9271a86535746ea
Luka1341 <113622133+Luka1341@users.noreply.github.com>
2022-11-21 21:36:07 +0100

    Delete functions6.js

--- functions6.js
+++
@@ -1,8 +0,0 @@
-﻿function fun(n) {
-    switch (n) {
-        case 1: document.getElementById("slike").src = "slike/sijalicaon.gif";
-            break;
-        case 2: document.getElementById("slike").src = "slike/sijalicaoff.gif";
-            break;
-    }
-}
