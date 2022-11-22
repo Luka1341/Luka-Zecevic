bb7fd08029015083f4773fede0465493a1bf919e
Luka1341 <113622133+Luka1341@users.noreply.github.com>
2022-11-22 20:40:38 +0100

    Add files via upload

---
+++ functions6.js
@@ -0,0 +1,8 @@
+﻿function fun(n) {
+    switch (n) {
+        case 1: document.getElementById("slike").src = "slike/sijalicaon.gif";
+            break;
+        case 2: document.getElementById("slike").src = "slike/sijalicaoff.gif";
+            break;
+    }
+}
